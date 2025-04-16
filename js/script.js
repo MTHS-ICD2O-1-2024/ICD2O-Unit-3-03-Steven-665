0e4debf6676a8d8230cbc5222818c18aeedc6460
Yi-Steven-Zhou <yi.zhou@stu.ocsb.ca>
2025-04-16 17:13:46 +0000

    fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: yi.zhou
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+
+
+"use strict"
+
+
+/**
+* This function calculates volume of a sphere.
+*/
+function calculateVolumeOfSphere () {
+ // input
+ const RadiusOfSphere  = parseFloat(document.getElementById('Radius-of-sphere').value)
+
+ // process
+ const volumeOfSphere = 4/3*Math.PI*RadiusOfSphere*RadiusOfSphere*RadiusOfSphere
+
+
+ // output
+ document.getElementById('volume').innerHTML = 'volume is: ' + volumeOfSphere.toFixed(2) + ' mm³'
+}
